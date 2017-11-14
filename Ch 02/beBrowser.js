6df7570ac0f794a2b9d818c3538fdf5380434bb0
Aseem Sharma <30532759+a5eeM@users.noreply.github.com>
2017-11-14 17:32:48 +0530

    Add Ch 02 code

---
+++ Ch 02/beBrowser.js
@@ -0,0 +1,26 @@
+// snipper 1
+var count = 0;
+for (var i = 0; i < 5; i++) {
+    count = count + i;
+}
+alert("count is" + count);
+
+// snippet 2
+var tops = 5;
+while (tops > 0) {
+    for (var spins = 0; spins < 3; spins++) {
+        alert("Top is spinning!");
+    }
+    tops = tops - 1;
+}
+
+// snippet 3
+for (var berries = 5; berries > 0; berries--) {
+    alert("Eating a berry");
+}
+
+// snippet 4
+for (var scoops = 0; scoops < 10; scoops++) {
+    alert("There's more ice cream!");
+}
+alert("life without ice cream isn't the same");
